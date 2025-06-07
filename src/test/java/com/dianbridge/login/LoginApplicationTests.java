f5cf694f4d4bce1f89372a3f65b2cfaa33ac887d
Diego Serna <juan.serna7836@unaula.edu.co>
2025-06-07 11:53:35 -0500

    Start project

---
+++ src/test/java/com/dianbridge/login/LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dianbridge.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
